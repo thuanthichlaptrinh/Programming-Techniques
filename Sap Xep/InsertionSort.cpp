fa0c6c01e8703863204bfa44287f9c201b760bb4
Ngô Minh Thuận <111260265+thuanthichlaptrinh@users.noreply.github.com>
2025-03-07 15:55:26 +0700

    Update InsertionSort.cpp

--- Sap Xep/InsertionSort.cpp
+++ Sap Xep/InsertionSort.cpp
@@ -10,7 +10,7 @@
 void insertionSort(int a[], int n){
     for(int i = 1; i < n; i++){
         int j = i;
-        while(j > 0 && a[j] <= a[j - 1]){
+        while(j > 0 && a[j] < a[j - 1]){
             int temp = a[j];
             a[j] = a[j - 1];
             a[j - 1] = temp;
